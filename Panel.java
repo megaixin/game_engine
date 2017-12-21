5ab179e130d22715fcbd8d9e474fb707bfe32959
megaixin <megaixin12@mail.ru>
2017-12-21 17:58:20 +0200

    autocommit with batch file 21.12.2017 17:58:20,66

---
+++ Panel.java
@@ -0,0 +1,22 @@
+﻿import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java. BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class myPanel {
+  
+    JFrame frame = new JFrame();
+    frame.setSize(100,100);
+       
+    JPanel panel = new JPanel();
+    panel.setSize(90,90);
+          
+    frame.add(panel);
+    
+	frame.setVisible(true);
+    panel.setVisible(true);
+
+}
