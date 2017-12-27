4a7daed4e067aab84d500632c84a3ef3aa54cd09
megaixin <megaixin12@mail.ru>
2017-12-27 15:36:41 +0200

    autocommit with batch file 27.12.2017 15:36:41,75

--- Java/myDrawingPanel.java
+++ Java/myDrawingPanel.java
@@ -2,32 +2,29 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class myDrawingPanel extends JPanel{
-	
-	
-	 @Override
-		public void paint(Graphics g){
-			super.paint(g);
-			g.drawLine(3, 20, 20, 20);
-	        g.drawLine(21, 20, 21, 35);
-	        g.drawLine(3, 35, 21, 35);
-	        g.drawLine(3, 35, 3, 20);
-			
-			//здесь будет вызов метода, к примеру: g.toDrawQuadrate(); (?)
-	 }
+  
+  
+  @Override
+  public void paint(Graphics g){
+    super.paint(g);
+    toDrawQuadrate(g, 20, 20);
+  }
 
-	 
-	 //нарисовать вадрат ( инт рассто€ние—верху, инт рассто€ние—боку){
-	 public static void toDrawQuadrate (int atopDist, int sideDist){
-		    
-		 //что здесь нужно сделать чтобы метод рисовал две линиии квадрата?
-		 //в теле метода тоже нужен пейнт?
-		 // как состыковать две линии?
-		 
-		 //и вот тут ебошить рисование квадрата
-		 //в нужном месте
-		 // а вызывать этот метод из paint
-		 
-	 }
-}
-		
+   
+   //нарисовать вадрат ( инт рассто€ние—верху, инт рассто€ние—боку){
+  public void toDrawQuadrate (Graphics g, int atopDist, int sideDist){
 
+	  
+    g.drawLine(3, 20, atopDist, sideDist);
+    //g.drawLine(21, 20, 21, 35);
+    //g.drawLine(3, 35, 21, 35);
+    //g.drawLine(3, 35, 3, 20);
+     
+   }
+}	
+
+
+
+//в чем хитрость аргумента Graphics g?
+//один вызов метода через пейнт рисует одну-единственную линию на панели
+//разберем еще раз где что рисветс€
