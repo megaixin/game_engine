7f914ecc59218eaf4276a21514ec28e4a5a8bd9a
megaixin <megaixin12@mail.ru>
2017-12-27 11:23:46 +0200

    autocommit with batch file 27.12.2017 11:23:46,08

--- Java/myDrawingPanel.java
+++ Java/myDrawingPanel.java
@@ -7,7 +7,27 @@ public class myDrawingPanel extends JPanel{
 	 @Override
 		public void paint(Graphics g){
 			super.paint(g);
-			g.drawLine(3, 15, 25, 34);
+			g.drawLine(3, 20, 20, 20);
+	        g.drawLine(21, 20, 21, 35);
+	        g.drawLine(3, 35, 21, 35);
+	        g.drawLine(3, 35, 3, 20);
+			
+			//здесь будет вызов метода, к примеру: g.toDrawQuadrate(); (?)
 	 }
-}			
+
+	 
+	 //нарисовать вадрат ( инт рассто€ние—верху, инт рассто€ние—боку){
+	 public static void toDrawQuadrate (int atopDist, int sideDist){
+		    
+		 //что здесь нужно сделать чтобы метод рисовал две линиии квадрата?
+		 //в теле метода тоже нужен пейнт?
+		 // как состыковать две линии?
+		 
+		 //и вот тут ебошить рисование квадрата
+		 //в нужном месте
+		 // а вызывать этот метод из paint
+		 
+	 }
+}
+		
 
