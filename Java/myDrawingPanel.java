483dfdf959b71f612f95c010a63085a1f497efed
megaixin <megaixin12@mail.ru>
2017-12-28 15:07:20 +0200

    autocommit with batch file 28.12.2017 15:07:20,09

--- Java/myDrawingPanel.java
+++ Java/myDrawingPanel.java
@@ -3,28 +3,37 @@ import javax.swing.JPanel;
 
 public class myDrawingPanel extends JPanel{
   
-  
   @Override
   public void paint(Graphics g){
     super.paint(g);
-    toDrawQuadrate(g, 20, 20);
+    //toDrawQuadrate(g, 20, 20);
+    //toDrawQuadrate(g, 20, 20);
+    //toDrawQuadrate(g, 21, 35);
+    //toDrawQuadrate(g, 3, 20);
+    toDrawLine (g, 5, 5);
+  
   }
 
    
-   //нарисовать вадрат ( инт рассто€ние—верху, инт рассто€ние—боку){
+  /* //нарисовать вадрат (инт рассто€ние—верху, инт рассто€ние—боку){
   public void toDrawQuadrate (Graphics g, int atopDist, int sideDist){
 
 	  
-    g.drawLine(3, 20, atopDist, sideDist);
-    //g.drawLine(21, 20, 21, 35);
-    //g.drawLine(3, 35, 21, 35);
-    //g.drawLine(3, 35, 3, 20);
+    g.drawLine(atopDist, sideDist, atopDist, sideDist);
+    g.drawLine(atopDist, sideDist, atopDist, sideDist);
+    g.drawLine(atopDist, sideDist, atopDist, sideDist);
+    g.drawLine(atopDist, sideDist, atopDist, sideDist);
      
    }
+*/
+  public void toDrawLine(Graphics g, int top, int left){
+
+	  g.drawLine(5, top, 5, left);
+	  
+	 }
+
+
+
 }	
 
 
-
-//в чем хитрость аргумента Graphics g?
-//один вызов метода через пейнт рисует одну-единственную линию на панели
-//разберем еще раз где что рисветс€
