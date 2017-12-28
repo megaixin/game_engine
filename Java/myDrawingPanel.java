ddafc01709fa28c4c8e8a8fde2814728d4116ad3
megaixin <megaixin12@mail.ru>
2017-12-28 15:54:26 +0200

    autocommit with batch file 28.12.2017 15:54:26,72

--- Java/myDrawingPanel.java
+++ Java/myDrawingPanel.java
@@ -6,34 +6,24 @@ public class myDrawingPanel extends JPanel{
   @Override
   public void paint(Graphics g){
     super.paint(g);
-    //toDrawQuadrate(g, 20, 20);
-    //toDrawQuadrate(g, 20, 20);
-    //toDrawQuadrate(g, 21, 35);
+     toDrawQuadrate(g, 20, 20);
+    // toDrawQuadrate(g, 35, 21);
     //toDrawQuadrate(g, 3, 20);
-    toDrawLine (g, 5, 5);
   
   }
 
    
-  /* //нарисовать вадрат (инт рассто€ние—верху, инт рассто€ние—боку){
+ //нарисовать вадрат (инт рассто€ние—верху, инт рассто€ние—боку){
   public void toDrawQuadrate (Graphics g, int atopDist, int sideDist){
 
-	  
-    g.drawLine(atopDist, sideDist, atopDist, sideDist);
-    g.drawLine(atopDist, sideDist, atopDist, sideDist);
-    g.drawLine(atopDist, sideDist, atopDist, sideDist);
-    g.drawLine(atopDist, sideDist, atopDist, sideDist);
-     
+    g.drawLine(10, atopDist, 15, sideDist);
+    // 10 - дистанци€ слева направо, от кра€ панели; 
+    //20(atopDist) - дист-€ от верха панели и вниз;
+    //15 - длинна линии вниз от точки 10; 20 - точка в которой будет завершено ведение линии 15
+    //g.drawLine(atopDist, sideDist, atopDist, sideDist);
+    //g.drawLine(10, atopDist, 15, sideDist);
    }
-*/
-  public void toDrawLine(Graphics g, int top, int left){
-
-	  g.drawLine(5, top, 5, left);
-	  
-	 }
-
-
 
 }	
 
-
+//почему их две? потому что одна запись о линии рисуетс€ полуквадратом (?)
