31ef64a0b4f65ef3aa50bc41d590f8e77369d0a2
megaixin <megaixin12@mail.ru>
2018-01-15 10:40:56 +0200

    autocommit with batch file 15.01.2018 10:40:56,05

--- Java/myDrawingPanel.java
+++ Java/myDrawingPanel.java
@@ -8,11 +8,12 @@ public class myDrawingPanel extends JPanel{
     super.paint(g);
 
     toDrawQuadrate(g, 15, 30);
+    toDrawHero (g, 15, 30);
   }
   
   public void toDrawQuadrate(Graphics g, int inX, int inY){//рисуем квадрат
       
-	  //базовый квадрат
+	 //базовый квадрат
 	 g.drawLine(inX, inY, inX+30, inY);
 	 g.drawLine(inX+30, inY, inX+30, inY+30);
 	  
@@ -30,10 +31,29 @@ public class myDrawingPanel extends JPanel{
 	   
 	 g.drawLine(inX+28, inY+23, inX+28, inY+28);//правая нижняя сторона, линия сверху вниз 
 	 g.drawLine(inX+28, inY+28, inX+23, inY+28);//правая нижняя сторона, линия справа налево 
-	   
-      //звездочка обозначающая героя
-	  //g.drawLine(inX+27, inY-27, inX+5, inY+10);//верхний луч, левая сторона снизу вверх
-	  //g.drawLine(inX+28, inY+28, inX+23, inY+28);//верхний луч, правая сторона сверху вниз
-	  
   }
+	 
+	 
+	  public void toDrawHero(Graphics g, int inX, int inY){//рисуем метку персонажа
+		  int[] myXArr = {inX-2,   inX+20,     inX+2,        inX+9,  inX+16};  
+	      int[] myYArr = {inY+2, inY+2,      inY+15,       inY-7,   inY+15};
+	      g.fillPolygon(myXArr, myYArr, myXArr.length);	  
+	  } 	 
+
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
 }
+
+
