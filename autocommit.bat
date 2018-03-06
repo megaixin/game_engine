a67e198ddb2f7b4fd5a346ba8de40d996a369e4c
megaixin <megaixin12@mail.ru>
2018-03-06 09:11:32 +0200

    autocommit with batch file 06.03.2018  9:11:32,19

--- autocommit.bat
+++ autocommit.bat
@@ -1,9 +1,6 @@
 rem @echo off
 
 
-git config --global user.email megaixin12@mail.ru
-git config --global user.password renerarr1209jnr
-
 git add --all
 git commit -m "autocommit with batch file %date% %time%"
 git push -u origin master
